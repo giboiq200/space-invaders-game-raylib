aea9899220a7386c9f7345286c07f55b879c2fe4
Bogdan <giboiq200@gmail.com>
2026-03-03 16:18:01 +0100

    Add project files.

---
+++ Project1/game1.cpp
@@ -0,0 +1,19 @@
+﻿#include <raylib.h>
+#include <iostream>
+
+int main() {
+	int width = 600;
+	int height = 600;
+	InitWindow(width,height,"Space invaders game");
+	SetTargetFPS(60);
+	//game loop
+	while (!WindowShouldClose()) {
+		//Update logic
+
+		BeginDrawing();
+		ClearBackground(BLACK);
+		EndDrawing();
+
+	}
+	CloseWindow();
+}
